d29ff42c1585260249618490e7ec11e059be9468
Danny Chin <c-wei@outlook.com>
2021-06-23 03:54:33 +1000

    added cypress test to assert tab order

--- cypress/integration/history-push-state.spec.js
+++ cypress/integration/history-push-state.spec.js
@@ -7,4 +7,19 @@ context('Accessible Tabs', () => {
         cy.injectAxe();
         cy.checkA11y();
     });
+
+    it('should tab works in correct order to activated tab control and panel', () => {
+        cy.get('body')
+            .tab()
+            .tab()
+            .should('have.class', 'tab-panel tab-panel--selected')
+            .and('contain.text', 'Tab 1-1 Lorem ipsum dolor sit amet,');
+    });
+
+    it('should tab pass first active tab group\'s control next active element will be 1st active panel', () => {
+        cy.focused()
+            .tab({ shift: true})
+            
+            
+    });
 });
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-axe'
+import 'cypress-plugin-tab'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
