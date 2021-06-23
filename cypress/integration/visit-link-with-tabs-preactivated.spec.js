f814b49034f0a083f6cfae00dc6d88e500d5b10c
Danny Chin <c-wei@outlook.com>
2021-06-24 04:58:36 +1000

    update page load state test to cover orientation attribute checkcs

--- cypress/integration/visit-link-with-tabs-preactivated.spec.js
+++ cypress/integration/visit-link-with-tabs-preactivated.spec.js
@@ -1,9 +1,9 @@
 context('When page visited with url contain activated tabs in query string', () => {
     before(() => {
-        cy.visit('/?tabs-1=tab-control-2&tabs-2=tab-control-5#tabs-2');
+        cy.visit('/?vertical-tabs-1=tab-control-vt-3&tabs-1=tab-control-2&tabs-2=tab-control-5#tabs-2');
     });
 
-    it('should first tab group activate correctly on page loaded', () => {
+    it('should first horizontal tab group activate correctly on page loaded', () => {
         cy.get('#tabs-1 .tab-control')
             .eq(1)
             .should('have.class', 'tab-control--selected')
@@ -13,10 +13,14 @@ context('When page visited with url contain activated tabs in query string', () 
         cy.get('#tabs-1 .tab-panel')
             .eq(1)
             .should('have.class', 'tab-panel--selected')
-            .not('have.attr', 'hidden')
+            .not('have.attr', 'hidden');
+
+        cy.get('#tabs-1 .tab-list')
+            .should('have.class', 'tab-list--horizontal')
+            .and('have.attr', 'aria-orientation', 'horizontal');
     });
 
-    it('should second tab group activate correctly on page loaded', () => {
+    it('should second horizontal tab group activate correctly on page loaded', () => {
         cy.get('#tabs-2 .tab-control')
             .eq(1)
             .should('have.class', 'tab-control--selected')
@@ -26,7 +30,28 @@ context('When page visited with url contain activated tabs in query string', () 
         cy.get('#tabs-2 .tab-panel')
             .eq(1)
             .should('have.class', 'tab-panel--selected')
-            .not('have.attr', 'hidden')
+            .not('have.attr', 'hidden');
+
+        cy.get('#tabs-2 .tab-list')
+            .should('have.class', 'tab-list--horizontal')
+            .and('have.attr', 'aria-orientation', 'horizontal');
+    });
+
+    it('should first vertical tab group activate correctly on page loaded', () => {
+        cy.get('#vertical-tabs-1 .tab-control')
+            .eq(2)
+            .should('have.class', 'tab-control--selected')
+            .and('have.attr', 'aria-selected', 'true')
+            .and('have.attr', 'tabindex', '0');
+
+        cy.get('#vertical-tabs-1 .tab-panel')
+            .eq(2)
+            .should('have.class', 'tab-panel--selected')
+            .not('have.attr', 'hidden');
+
+        cy.get('#vertical-tabs-1 .tab-list')
+            .should('have.class', 'tab-list--vertical')
+            .and('have.attr', 'aria-orientation', 'vertical');
     });
 
     it('should pass accessibility test', () => {
